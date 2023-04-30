ca8d60582cf2e7029aa90998d3e8becaec4bc9c6
Bridge Nyagwencha <nyagwenchar@gmail.com>
2023-04-30 05:09:54 +0300

    Update script.py

--- script.py
+++ script.py
@@ -54,7 +54,7 @@ Source: <a href='https://github.com/Nyagwenchar/AutoFilterBot - Click here 👈<
 3. Buttons should be properly parsed as markdown format.
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/josprojects)</code>
+<code>[Button Text](buttonurl:https://t.me/nyagwenchar)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
